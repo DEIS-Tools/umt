804ad1aac03a6f71da198f0e0d5c849f03ec294d
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2021-01-08 12:35:24 +0100

    Arrows are now clickable

--- UMT/src/ts/editor/Arrow.ts
+++ UMT/src/ts/editor/Arrow.ts
@@ -2,13 +2,24 @@
 /// drawing libraries easier.
 
 // TODO: This should be done via implementation classes instead.
+type yes = HTMLElement | null;
 class Arrow {
     private graphics: LeaderLine;
+    private svgElem: HTMLElement | null;
 
     constructor(start: HTMLElement, end: HTMLElement) {
-        this.graphics = new LeaderLine(start, end); // TODO: Styling
+        this.graphics = new LeaderLine(start, end, {path: "arc"}); // TODO: User styling
+        this.svgElem = document.querySelector('body>.leader-line:last-of-type');
+        if(!this.svgElem) {
+            console.log("Something went wrong. Expect weird behavior");
+            return;
+        }
+        this.svgElem.addEventListener("click", this.editGraphics.bind(this));
     }
     public UpdateGraphics() {
         this.graphics.position();
+    }
+    public editGraphics(e: Event) {
+        console.log("You pressed an arrow!");
     }
 };
